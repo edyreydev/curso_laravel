300826059db95c83999440cdf103e62fe1044acf
edyreydev <edyrey0101@gmail.com>
2023-10-06 06:07:28 -0500

    Modelos en Laravel

--- database/migrations/2023_10_06_042022_create_alumnos_table.php
+++ database/migrations/2023_10_06_042022_create_alumnos_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('alumnos', function (Blueprint $table) {
             $table->id();
+            $table->string('nombre');
+            $table->string('apellido');
+            $table->string('edad');
+            $table->string('direccion');
             $table->timestamps();
         });
     }
